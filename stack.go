b9c48e2bea65cc0d9a9872cbd03122fcea5df7c0
Julian Sia <siajat@utexas.edu>
2017-08-23 22:37:50 -0500

    fleshed out stack stubs

--- stack.go
+++ stack.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"errors"
 )
 
 type Node struct {	
@@ -19,11 +20,23 @@ type StackOps interface {
 }
 
 func (stack *Stack) push(val int) {
-	
+	n := Node{val, nil}
+	if stack.top == nil {	
+		stack.top = &n
+	} else {
+		n.next = stack.top
+		stack.top = &n
+	}
 }
 
 func (stack *Stack) pop() *Node {
-	
+	if stack.top == nil {
+		empty_stack_error := errors.New("Pop from empty stack!")
+		fmt.Println(empty_stack_error)
+	} 
+	return_node := stack.top
+	stack.top = stack.top.next
+	return return_node
 }
 
 
