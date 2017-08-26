d0fb94737f1a882375a9c55a4a97c8159ddf3068
Julian Sia <siajat@utexas.edu>
2017-08-25 19:43:54 -0500

    stub out tests for stack

---
+++ stack_test.go
@@ -0,0 +1,26 @@
+package "main"
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestPush(t *testing.T) {
+
+}
+
+func TestPop(t *testing.T) {
+
+}
+
+func TestPopEmpty(t *testing.T) {
+
+}
+
+func TestPushThenPop(t *testing.T) {
+
+}
+
+func TestPopThenPush(t *testing.T) {
+	
+}
