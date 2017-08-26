44dedbdcc6791ea6762b426c32dcc20dce3bc364
Julian Sia <siajat@utexas.edu>
2017-08-26 13:53:25 -0500

    stubs for linked list tests

---
+++ linked_list/linked_list_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_Add(t *testing.T) {
+	
+}
+
+func Test_RemoveTail(t *testing.T) {
+
+}
+
+func Test_RemoveHead(t *testing.T) {
+
+}
+
+func Test_AddAddRemoveHeadAdd(t *testing.T) {
+
+}
+
+func Test_AddAddRemoveTailAdd(t *testing.T) {
+
+}
+
+func Test_RemoveHeadEmpty(t *testing.T) {
+
+}
+
+func Test_RemoveTailEmpty(t *testing.T) {
+
+}
+
+func Test_RandomAddRemove_1(t *testing.T) {
+
+}
+
+func Test_RandomAddRemove_2(t *testing.T) {
+
+}
+
+func Test_RandomAddRemove_3(t *testing.T) {
+
+}
