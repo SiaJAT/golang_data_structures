361bf2684aafcf43c0e8768260d504c205dd601f
Julian Sia <siajat@utexas.edu>
2017-08-27 21:13:17 -0500

    more stubs for fns

--- array_list/array_list.go
+++ array_list/array_list.go
@@ -17,10 +17,13 @@ type List interface {
 	add(value int)
 	remove(index int)
 	removeLast()
+	getIterableArray() []int
 }
 
 func (list *ArrayList) init() {
 	list.container = make([]int, 10)
+	list.size = 10
+	list.end = 0
 }
 
 func (list *ArrayList) add(value int) (err error) {
@@ -31,6 +34,20 @@ func (list *ArrayList) add(value int) (err error) {
 	list.container[list.end] = value
 	list.end += 1
 	return nil
+}
+
+func (list *ArrayList) remove(index int) {
+	
+}
+
+func (list *ArrayList) removeLast() {
+	if end > 0 {
+		end -= 1
+	}
+}
+
+func (list *ArrayList) getIterableArray() []int {
+	return list.container[0:end]
 }
 
 
