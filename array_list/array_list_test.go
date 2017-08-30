73c8a597fc977b909c54aead62a7de4e4e8acfb3
Julian Sia <siajat@utexas.edu>
2017-08-29 21:12:21 -0500

    minimal contributions lol

--- array_list/array_list_test.go
+++ array_list/array_list_test.go
@@ -5,31 +5,31 @@ import (
 )
 
 func Test_Add(t *testing.T) {
-
+	list := ArrayList{}
 }
 
 
 func Test_Remove(t *testing.T) {
-
+	list := ArrayList{}
 }
 
 func Test_AddAddRemoveAdd(t *testing.T) {
-
+	list := ArrayList{}
 }
 
 func Test_RemoveEmpty(t *testing.T) {
-
+	list := ArrayList{}
 }
 
 
 func Test_RandomAddRemove_1(t *testing.T) {
-
+	list := ArrayList{}
 }
 
 func Test_RandomAddRemove_2(t *testing.T) {
-
+	list := ArrayList{}
 }
 
 func Test_RandomAddRemove_3(t *testing.T) {
-
+	list := ArrayList{}
 }
